a7ace0c6204c6d00b33faa5b4fc86a08265a3547
Petr Stepanov <kisstherail@gmail.com>
2016-12-14 05:53:27 -0500

    tweaks

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
         <meta name="description" content="<?php echo get_bloginfo( 'description' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/selimlab.css?t=1481710730585">
+        <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/selimlab.css?t=1481712695528">
         <link rel="shortcut icon" href="favicon.ico">
         <?php wp_head(); ?>
     </head>
